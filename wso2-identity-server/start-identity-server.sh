e7d6137243752b7918b39d96ebd02ba5b8d5128f
sahan <sahanruwanga.15@cse.mrt.ac.lk>
2020-04-09 01:07:02 +0530

    Mount a directory for deployment.toml

--- wso2-identity-server/start-identity-server.sh
+++ wso2-identity-server/start-identity-server.sh
@@ -4,6 +4,7 @@ WSO2IS_CONTAINER=training_wso2is
 
 docker run -ti \
     -p 9443:9443 \
+    -v "$(pwd)"/conf/deployment.toml:/home/wso2carbon/wso2is-5.10.0/repository/conf/deployment.toml \
     --name ${WSO2IS_CONTAINER} \
-    wso2/wso2is
+    wso2/wso2is:5.10.0
 
